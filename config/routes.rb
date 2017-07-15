00aa5208f2651f98b934342c74e5c1b49a977b88
Mariusz Błaszczak <mariusz.blaszczak@gmail.com>
2017-07-15 12:40:19 +0300

    point to root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'hello_linuxpl/index'
-
+  root to: "hello_linuxpl#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
